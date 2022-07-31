331704b196d8a0af04385b3c147a97471750b43e
Cursedmouth <piotr.jarosz.87@gmail.com>
2022-07-31 20:59:53 +0200

    4. Question Dao

---
+++ src/main/java/pl/edu/wszib/quiz/dao/QuestionDao.java
@@ -0,0 +1,9 @@
+package pl.edu.wszib.quiz.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import pl.edu.wszib.quiz.model.Question;
+
+@Repository
+public interface QuestionDao extends CrudRepository<Question, Integer> {
+}

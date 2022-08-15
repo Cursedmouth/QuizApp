9858437004390b8fbe5aecbcd2949371ed334249
Cursedmouth <piotr.jarosz.87@gmail.com>
2022-08-15 15:48:08 +0200

    7. PostreSQL - heroku settings

--- src/main/java/pl/edu/wszib/quiz/controller/HomeController.java
+++ src/main/java/pl/edu/wszib/quiz/controller/HomeController.java
@@ -1,13 +1,19 @@
 package pl.edu.wszib.quiz.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Value("${app.title.homePage}")
+    private String title;
+
     @GetMapping
-    public String showHomePage() {
+    public String showHomePage(Model model) {
+        model.addAttribute("title", title);
         return "homePage";
     }
 }
